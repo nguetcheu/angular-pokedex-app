196afa06b187fdb6a72c5d081dce67b5d89f3fec
nguetcheu <dnguetcheu@gmail.com>
2024-08-29 18:07:23 +0200

    Pokedex + signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-pokedex-app';
+  name = signal('Pikachu');
+  life = signal(21); // signal de base et un signal est une fonction
+  taille = computed(() => {
+    if (this.life() <= 15) {
+      return 'Petit';
+    }
+
+    if (this.life() > 15 && this.life() < 25) {
+      return 'Normal';
+    }
+    return 'Grand';
+  });
+
+  doubleLife = computed(() => this.life() * 2); // signal dérivé
+
+  constructor() {
+    effect(() => {
+      console.log('La vie du pokémon a été mis à jour :', this.life());
+    }); // Permet d'interagir avec toute les mises a jour d'un signal
+  }
+
+  incrementLife() {
+    this.life.update((n) => n + 1); // update permet de mettre a jour l'état d'un signal avec une fonction
+  }
+
+  decrementLife() {
+    this.life.update((n) => n - 1);
+  }
 }
